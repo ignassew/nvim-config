8afda9e3ad81312a45f72e6556aaa89829500b26
ignassew <szpiren@gmail.com>
2023-01-28 19:28:20 +0100

    Remove onedark plugin

--- init.lua
+++ init.lua
@@ -48,7 +48,6 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use { "catppuccin/nvim", as = "catppuccin" }
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
